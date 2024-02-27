0983fc6e267c5109683036b6742a0aa024dc4313
KaviHT <kaviht1001@gmail.com>
2024-02-27 22:47:22 +0530

    Get node and flask servers running

--- node_backend/src/app.js
+++ node_backend/src/app.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 const port = process.env.PORT || 3000;
 const routes = require('./routes');
 
 app.use(express.json());
+app.use(fileUpload({
+     useTempFiles: true, // This option will store uploaded files in temporary directory
+     tempFileDir: '/tmp/' // Temporary directory path
+}));
 app.use('/', routes);
 
 app.listen(port, () => {
--- node_backend/src/controllers/predictionsController.js
+++ node_backend/src/controllers/predictionsController.js
@@ -1,10 +1,23 @@
 const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
 
 exports.getPredictions = async (req, res) => {
+     if (!req.files || Object.keys(req.files).length === 0) {
+          return res.status(400).send('No files were uploaded.');
+      }
+  
+      // Access the uploaded file
+      let file = req.files.file; // Use of middleware express-fileupload to parse files
+  
+      // Prepare the form data
+      let formData = new FormData();
+      formData.append('file', fs.createReadStream(file.tempFilePath)); // Adjust based on how you access the file
+  
     try {
-        const response = await axios.post(process.env.FLASK_SERVICE_URL, req.body, {
+        const response = await axios.post(process.env.FLASK_SERVICE_URL, formData, {
             headers: {
-                'Content-Type': 'multipart/form-data',
+               ...formData.getHeaders(),
             },
         });
         res.json(response.data);
