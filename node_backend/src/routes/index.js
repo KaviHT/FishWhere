66e62fa8258d2252f8ab15fc81c9bcf4327b9648
KaviHT <kaviht1001@gmail.com>
2024-02-28 10:35:33 +0530

    Setup fileupload with file names in "uploads"

--- node_backend/src/routes/index.js
+++ node_backend/src/routes/index.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const multer = require('multer');
 const predictionsController = require('../controllers/predictionsController');
 
+const storage = multer.diskStorage({
+     destination: function (req, file, cb) {
+          cb(null, 'uploads/');
+     },
+     filename: function (req, file, cb) {
+          cb(null, file.fieldname + '-' + Date.now());
+     }
+})
+
 // Configure multer for file uploads
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ storage: storage});
 
 router.post('/predict', upload.single('file'), predictionsController.getPredictions);
 
