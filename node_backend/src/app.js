be9efd15c382eddf330cd8acbf8b5d1709a6a8f8
KaviHT <kaviht1001@gmail.com>
2024-03-05 17:57:48 +0530

    Connect node backend with flutter frontend

--- node_backend/src/app.js
+++ node_backend/src/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require('./routes');
 
 app.use(express.json());
+app.use(cors());
 app.use('/', routes);
 
 app.listen(PORT, () => {
