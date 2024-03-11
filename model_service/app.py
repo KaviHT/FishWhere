3b9934a0e4cf4369e5c2b7e67218c8afd64d8830
KaviHT <kaviht1001@gmail.com>
2024-03-11 23:37:07 +0530

    Make the script compatible with the new model

--- model_service/app.py
+++ model_service/app.py
@@ -9,7 +9,7 @@ import os
 app = Flask(__name__)
 
 # Load the machine learning model
-MODEL_PATH = './model/RandomForest3.joblib'
+MODEL_PATH = './model/voting_clf_sknew.joblib'
 model = joblib.load(MODEL_PATH)
 
 
@@ -28,17 +28,17 @@ def predict():
                
                # --- Preprocess the data as needed and make predictions ---
 
-               # Convert 'date' column to 'dayofyear'
-               df['dayofyear'] = pd.to_datetime(df['date']).dt.dayofyear
-               # Drop the original 'date' column
-               df.drop(columns=['date'], inplace=True)
+               # # Convert 'date' column to 'dayofyear'
+               # df['dayofyear'] = pd.to_datetime(df['date']).dt.dayofyear
+               # # Drop the original 'date' column
+               # df.drop(columns=['date'], inplace=True)
 
                # Select the input features
-               # input_features = df[['lat', 'lon', 'sst', 'chl']]
+               input_features = df[['lat', 'lon', 'sst', 'chl']]
 
                # After the data is ready for prediction
-               # predictions = model.predict(input_features)
-               predictions = model.predict(df)
+               predictions = model.predict(input_features)
+               # predictions = model.predict(df)
 
                # Checking the count of 0s and 1s in the predictions
                unique, counts = np.unique(predictions, return_counts=True)
